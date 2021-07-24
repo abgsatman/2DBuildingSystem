57b7127f072b7c611134b88e02ee814bec26f371
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-07-24 19:55:45 +0300

    Build it!

---
+++ 2D Building System/Assets/Scripts/BuildUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildUp : MonoBehaviour
+{
+    public void BuildUpProcess()
+    {
+        Sprite selectedSprite = GameObject.Find("GameManager").GetComponent<GameManager>().selectedSprite;
+        Image selectedObject = Instantiate(GameObject.Find("GameManager").GetComponent<GameManager>().building, this.gameObject.transform);
+
+        if (selectedSprite != null)
+        {
+            //replace sprites
+            //this.gameObject.GetComponent<Image>().sprite = selectedSprite;
+
+            selectedObject.GetComponent<Image>().sprite = selectedSprite;
+        }
+        else
+            Debug.LogError("<size=10>You have to select an inventory first!</size>");
+    }
+}
