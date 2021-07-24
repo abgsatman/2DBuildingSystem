51dc0c64bd0c8aab8ca03554bce9cb24d242d735
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-07-24 17:49:21 +0300

    Dynamic grid system

---
+++ 2D Building System/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+
+public class GameManager : MonoBehaviour
+{
+    //canvas object
+    public GameObject parentObject;
+
+    //prefab object
+    public Image building;
+
+    [Range(1, 10)]
+    public int gridX, gridY;
+
+    [SerializeField]
+    private float buildingCurrentSizeX, buildingCurrentSizeY;
+
+    [SerializeField]
+    private float defaultPositionX, defaultPositionY;
+    
+    void Start()
+    {
+        buildingCurrentSizeX = building.GetComponent<RectTransform>().sizeDelta.x;
+        buildingCurrentSizeY = building.GetComponent<RectTransform>().sizeDelta.y;
+
+        defaultPositionX = building.GetComponent<RectTransform>().anchoredPosition.x;
+        defaultPositionY = building.GetComponent<RectTransform>().anchoredPosition.y;
+
+        GridSystem();
+    }
+
+    void GridSystem()
+    {
+        for(int i = 0; i < gridX; i++)
+        {
+            for(int j = (gridY - 1); j >=0; j--)
+            {
+                Image image = Instantiate(building, parentObject.transform);
+                image.GetComponent<RectTransform>().anchoredPosition = new Vector3(defaultPositionX + (buildingCurrentSizeX * i), defaultPositionY - (buildingCurrentSizeY * j), 0);
+
+                var buildingName = new StringBuilder();
+                buildingName.Append("Building");
+                buildingName.Append(i);
+                buildingName.Append("_");
+                buildingName.Append(j);
+
+                image.name = buildingName.ToString();
+            }
+        }
+    }
+}
